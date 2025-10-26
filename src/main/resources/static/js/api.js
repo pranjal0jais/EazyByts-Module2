7f2d8719f2ed6670a159c83d9bdc9fc2e95decf5
Pranjal Jais <pranjaljais2@gmail.com>
2025-10-26 18:00:54 +0530

    base url changes in  api.js

--- src/main/resources/static/js/api.js
+++ src/main/resources/static/js/api.js
@@ -1,5 +1,5 @@
 // js/api.js
-const API_BASE_URL = 'https://stock-trading-simulator-production.up.railway.app';
+const API_BASE_URL = 'http://localhost:8080';
 
 async function apiCall(endpoint, options = {}) {
     const token = getToken?.();
